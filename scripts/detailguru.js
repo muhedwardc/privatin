c52c1e60aba1a6b14776ba6d5d7aefa8015cea61
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-04-17 17:16:49 +0700

    updating detailguru

--- scripts/detailguru.js
+++ scripts/detailguru.js
@@ -1,25 +1,35 @@
-const pilihanHari = document.getElementById('pilihan-hari'),
-      pilihanJam = document.getElementById('pilihan-jam'),
+const pilihanPaket = document.getElementById('pilihan-paket'),
+      jumlahPertemuan = document.getElementById('jumlah-pertemuan'),
+      pilihanJadwal = document.getElementById('pilihan-jadwal'),
       jadwalTerpilih = document.querySelector('.jadwal-terpilih'),
       pesanJadwalBtn = document.querySelector('.pesan-jadwal'),
-      rangkumanHari = document.querySelector('.summary-hari'),
-      rangkumanJam = document.querySelector('.summary-jam');
+      rangkumanJadwal = document.querySelector('.summary-jadwal'),
+      spanPaketTerpilih = document.querySelector('.paket-terpilih'),
+      spanPertemuanTerpilih = document.querySelector('.pertemuan-terpilih');
 
 let arrJadwalTerpilih = [];
+let paketValue = {
+    sd: [50000, 60000, 70000],
+    smp: [70000, 80000, 90000],
+    sma: [90000, 100000, 100000]
+};
 
 tambahJadwal = () => {
-    let tambahHari = pilihanHari.options[pilihanHari.selectedIndex].text;
-    let tambahJam = pilihanJam.options[pilihanJam.selectedIndex].text;
-    let inText = tambahHari + tambahJam;
-    let terpilih = tambahHari + '+' + tambahJam;
+    pilihanPaket.disabled = true;
+    jumlahPertemuan.disabled = true;
+    paket = pilihanPaket.options[pilihanPaket.selectedIndex].value;
+    let jmlPertemuan = jumlahPertemuan.options[jumlahPertemuan.selectedIndex].text;
+    let jadwal = pilihanJadwal.options[pilihanJadwal.selectedIndex].text;
+
     let ada = false;
     
     for (let i = 0; i < arrJadwalTerpilih.length; i ++) {
-        arrJadwalTerpilih[i].toString().includes(terpilih) ? ada = true : null;
+        arrJadwalTerpilih[i].toString().includes(jadwal) ? ada = true : null;
     }
 
     if (!ada) {
-        arrJadwalTerpilih.push(terpilih);
+        arrJadwalTerpilih.push(jadwal);
+        console.log(arrJadwalTerpilih);
         updateView();
     } else if (ada && arrJadwalTerpilih.length != 0){
         alert ('Kowe wis milih kuwi lee');
@@ -28,9 +38,13 @@ tambahJadwal = () => {
 
 ulClicked = () => {
     if (event.target.tagName === 'BUTTON') {
-        let li = event.target.parentNode;
+        let li = event.target.parentNxode;
         let urutan = Number(event.path[1].attributes[1].value);
         arrJadwalTerpilih.splice(urutan, 1);
+        if (arrJadwalTerpilih.length === 0) {
+            pilihanPaket.disabled = false;
+            jumlahPertemuan.disabled = false;
+        }
         updateView();
     }
 }
@@ -38,10 +52,7 @@ ulClicked = () => {
 updateView = () => {
     jadwalTerpilih.innerHTML = null;
     for (let i = 0; i < arrJadwalTerpilih.length; i ++) {
-        let splitArr = arrJadwalTerpilih[i].split('+');
-        let html = '<span class="hari-terpilih">' 
-            + splitArr[0] + '</span><span class="jam-terpilih">' 
-            + splitArr[1] + '</span><button class="hapus-btn title="hapus">&#x2716;</button>';
+        let html = '<div class="list-jadwal-item">' +  arrJadwalTerpilih[i] + '</div><button class="hapus-btn" title="hapus jadwal">Hapus</button>';
         const listTambah = document.createElement('li');
         listTambah.setAttribute('class', 'item-tambah');
         listTambah.setAttribute('urutan', i);
@@ -50,27 +61,24 @@ updateView = () => {
     }
 }
 
-rangkumanJadwal = () => {
-    rangkumanHari.innerHTML = '';
-    rangkumanJam.innerHTML = '';
+checkout = () => {
+
+    rangkumanJadwal.innerHTML = '';
     if (arrJadwalTerpilih.length === 0) {
         alert('durung di isi leeee, kowe milih jadwal opo?');
         pesanJadwalBtn.setAttribute('data-toggle', '');
         pesanJadwalBtn.setAttribute('data-target', '');
     } else {
+        spanPaketTerpilih.textContent = pilihanPaket.options[pilihanPaket.selectedIndex].text;
+        spanPertemuanTerpilih.textContent = jumlahPertemuan.options[jumlahPertemuan.selectedIndex].text;
+
         pesanJadwalBtn.setAttribute('data-toggle', 'modal');
         pesanJadwalBtn.setAttribute('data-target', '#summary');
-
         for (let i = 0; i < arrJadwalTerpilih.length; i++) {
-            let listHari = document.createElement('li');
-            let listJam = document.createElement('li');
-            let jadwal = arrJadwalTerpilih[i].split('+');
-            let hari = jadwal[0],
-                jam = jadwal[1];
-            listHari.innerHTML = hari;
-            listJam.innerHTML = jam;
-            rangkumanHari.appendChild(listHari);
-            rangkumanJam.appendChild(listJam);
+            let listJadwal = document.createElement('li');
+            let jadwalTerpilih = arrJadwalTerpilih[i];
+            listJadwal.innerHTML = jadwalTerpilih;
+            rangkumanJadwal.appendChild(listJadwal);
         }
     }
 }
